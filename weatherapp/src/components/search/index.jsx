9590b05ce1d4672364bc3527789228f6c88c7f84
Vishakha-Kolapkar-V2 <103262109+Vishakha-Kolapkar-V2@users.noreply.github.com>
2024-05-06 12:48:55 +0530

    added styling, and functionality

--- weatherapp/src/components/search/index.jsx
+++ weatherapp/src/components/search/index.jsx
@@ -6,13 +6,12 @@ export default function Search({search, setSearch,handleSearch}) {
         
         <input
         type="text"
-        className='city-search'
         placeholder='Enter city Name'
         name='search'
         value={search}
         onChange={(event)=>setSearch(event.target.value)}
         />
-        <button className='search-btn' onClick={handleSearch}>Search Weather</button>
+        <button onClick={handleSearch}>Search Weather</button>
       </div>
     )
   }
--- weatherapp/src/components/weather/index.jsx
+++ weatherapp/src/components/weather/index.jsx
@@ -1,12 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from '../search'
 
 export default function Weather() {
-  return (
-    <div>
-        <Search/>
-      <h1>weather</h1>
-    </div>
-  )
+    const [search, setSearch] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [weatherData, setWeatherData] = useState(null);
+
+    async function fetchWeatherData(param) {
+        setLoading(true)
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=e49321e5ffe86db3f30a7ae0e870abbf`);
+
+            const data = await response.json();
+            console.log(data, 'data');
+            if (data) {
+                setWeatherData(data);
+                setLoading(false);
+            }
+        } catch (e) {
+            setLoading(false);
+            console.log(e);
+        }
+    }
+
+    function handleSearch() {
+        fetchWeatherData(search);
+    }
+
+    function getCurrentDate() {
+        return new Date().toLocaleDateString('en-us', {
+            weekday: 'long',
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric'
+        })
+    }
+    useEffect(() => {
+        fetchWeatherData(`bangalore`)
+    }, [])
+
+    console.log(weatherData)
+
+    return (
+        <div>
+            <Search search={search} setSearch={setSearch} handleSearch={handleSearch} />
+            {
+                loading ? <div className='loading'>Loading....</div> : <div><div className='city-name'>
+                    <h2>
+                        {weatherData?.name}, <span>{weatherData?.sys?.country}</span>
+                    </h2>
+                </div>
+                    <div className='date'>
+                        <span>{getCurrentDate()}</span>
+                    </div>
+                    <div className='tempreture'>{weatherData?.main?.temp}</div>
+                    <p className='description'>{weatherData && weatherData.weather[0] ? weatherData.weather[0].description : ''}</p>
+                    <div className='weather-info'>
+                        <div>
+                            <div className='column'>
+                                <p className='wind'>{weatherData?.wind?.speed}</p>
+                                <p>Wind Speed</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='column'>
+                                <p className='humidity'>{weatherData?.main?.humidity}%</p>
+                                <p>Humidity</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
+        </div>
+    )
 }
 
