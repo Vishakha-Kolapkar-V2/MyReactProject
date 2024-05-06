9e99f8f2f7c1a5ca93b8a48c2758df26d4c2c478
Vishakha-Kolapkar-V2 <103262109+Vishakha-Kolapkar-V2@users.noreply.github.com>
2024-05-06 11:00:12 +0530

    Initial commit

---
+++ weatherapp/src/components/search/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Search({search, setSearch,handleSearch}) {
+    return (
+      <div className='search-engine'>
+        
+        <input
+        type="text"
+        className='city-search'
+        placeholder='Enter city Name'
+        name='search'
+        value={search}
+        onChange={(event)=>setSearch(event.target.value)}
+        />
+        <button className='search-btn' onClick={handleSearch}>Search Weather</button>
+      </div>
+    )
+  }
+  
---
+++ weatherapp/src/components/weather/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Search from '../search'
+
+export default function Weather() {
+  return (
+    <div>
+        <Search/>
+      <h1>weather</h1>
+    </div>
+  )
+}
+
