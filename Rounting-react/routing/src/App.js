7488ae48bfa4c346cdb5c0d51e8382c5c31ee471
Vishakha-Kolapkar-V2 <103262109+Vishakha-Kolapkar-V2@users.noreply.github.com>
2024-05-09 17:26:41 +0530

    routing

---
+++ Rounting-react/routing/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './cmd/Home';
+import About from './cmd/About';
+import Forms from './cmd/Forms';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <About/>
+      <Forms/>
+    </div>
+  );
+}
+
+export default App;
