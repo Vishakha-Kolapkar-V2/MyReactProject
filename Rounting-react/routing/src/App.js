9219a75a8ee57b346241732631b0db3285771367
Vishakha-Kolapkar-V2 <103262109+Vishakha-Kolapkar-V2@users.noreply.github.com>
2024-05-09 18:11:00 +0530

    Handson routing concept

--- Rounting-react/routing/src/App.js
+++ Rounting-react/routing/src/App.js
@@ -1,15 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Home from './cmd/Home';
 import About from './cmd/About';
 import Forms from './cmd/Forms';
+import {BrowserRouter as Router, Route,Routes, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <Link to="">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/forms">Forms</Link>
+      <Routes>
+      <Route path='' Component={Home} />
+      <Route path='/about' Component={About} />
+      <Route path='/forms' Component={Forms} />
+      </Routes>
+        
+      {/* <Home/>
       <About/>
-      <Forms/>
+      <Forms/> */}
+      </Router>
     </div>
   );
 }
